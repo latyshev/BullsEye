09c7b81f50d7f73713eb3118f81cfefb28cef4c1
latyshev <latyshev831@gmail.com>
2019-06-07 19:51:28 +0300

    chapter 6 done

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -20,21 +20,35 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    startNewRound()
+    startNewGame()
   }
     
   @IBAction func showAlert() {
     let difference = abs(targetValue - currentValue)
-    let points = 100 - difference
+    var points = 100 - difference
     
+    let title: String
+    if difference == 0 {
+      title = "Perfect!"
+      points += 100
+    } else if difference < 5 {
+      title = "You almost had it!"
+      if difference == 1 {
+        points += 50
+      }
+    } else if difference < 10 {
+      title = "Pretty good!"
+    } else {
+      title = "Not even close..."
+    }
     score += points
     
     let message = "You scored \(points) points"
-    let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
-    let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+    
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: "Ok", style: .default, handler: { _ in self.startNewRound()})
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
-    startNewRound()
   }
     
   @IBAction func sliderMoved(_ slider: UISlider) {
@@ -53,6 +67,12 @@ class ViewController: UIViewController {
     targetLabel.text = String(targetValue)
     scoreLabel.text = String(score)
     roundLabel.text = String(round)
+  }
+  
+  @IBAction func startNewGame() {
+    score = 0
+    round = 0
+    startNewRound()
   }
 }
 
